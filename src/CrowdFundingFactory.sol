7198022ef2373af5a2d287ec1f8e9db7774543af
somtech123 <oscardre8@gmail.com>
2026-03-18 07:04:01 -0700

    added event to the create campaign functions

--- src/CrowdFundingFactory.sol
+++ src/CrowdFundingFactory.sol
@@ -10,24 +10,32 @@ contract CrowdFundingFactory{
     uint256 campaignCounter;
     uint256 constant public MINIMUM_USD = 1e16;
     uint256 constant public CREATION_FEE = 0.010 ether;
-   
+
+
+   event CampaignCreated(address indexed creator, uint256 feesPaid, uint256 campaignId);
 
     struct CampaignInfo{
         address campaignAddress;
         address creator;
         uint256 goal;
+        uint256 createdAt;
     }
-    mapping (uint256 => CampaignInfo) public campaign;
-    mapping (address => uint256) public campaignCreated;
+    mapping (uint256 => CampaignInfo) public campaigns;
 
+   
     function createCampaign( uint256 _goal, uint256 _deadine) external payable   {
-        uint256 daysValue = _deadine * 1 days;
+        uint256 durationInDays = _deadine * 1 days;
          
         if(_goal == 0 ) revert InvalidGoal();
+
         if(_goal < MINIMUM_USD) revert LessThanMinimumGoal();
-        if(daysValue == 0) revert InvalidDeadLine();
-        if(daysValue <=  1 days) revert DeadLineTooClose();
-        if(daysValue >=  365 days ) revert DeadLineToFar();
+
+        if(durationInDays == 0) revert InvalidDeadLine();
+
+        if(durationInDays <=  1 days) revert DeadLineTooClose();
+
+        if(durationInDays >=  365 days ) revert DeadLineToFar();
+
         if(msg.value <= CREATION_FEE) revert InsufficientFee();
         
         uint256 goal = UnitConverter.toWei(_goal);
@@ -38,13 +46,16 @@ contract CrowdFundingFactory{
             _deadine
         );
 
-        campaign[campaignCounter] = CampaignInfo({
+        campaigns[campaignCounter] = CampaignInfo({
             campaignAddress: address(_campaign),
             creator: msg.sender,
-            goal: goal
+            goal: goal,
+            createdAt: block.timestamp
         });
 
         campaignCounter++;
+
+        emit CampaignCreated(msg.sender, msg.value, campaignCounter);
     
     }
 
