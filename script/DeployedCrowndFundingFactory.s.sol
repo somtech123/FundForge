269db66ca53c68e1906069b7053dfde680207fa6
somtech123 <oscardre@gmail.com>
2026-03-26 21:57:13 -0700

    wrote deployment script for crowndfunding factory

--- script/DeployedCrowndFundingFactory.s.sol
+++ script/DeployedCrowndFundingFactory.s.sol
@@ -2,14 +2,29 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
 
 import {CrowdFundingFactory} from "../src/CrowdFundingFactory.sol";
 
 contract DeployedCrowndFundingFactory is Script {
     function run() external returns (CrowdFundingFactory) {
-        vm.startBroadcast();
+        uint256 chainId = block.chainid;
 
-        CrowdFundingFactory factory = new CrowdFundingFactory();
+        CrowdFundingFactory factory;
+        vm.startBroadcast();
+        if (chainId == 11155111) {
+            //run on sepolia testnet
+            factory = new CrowdFundingFactory();
+        } else if (chainId == 1) {
+            //run on etherum mainnet
+            factory = new CrowdFundingFactory();
+        } else if (chainId == 31337) {
+            //run on anvil local testnet
+            factory = new CrowdFundingFactory();
+        } else {
+            //  only fires if NO chain matched
+            revert("Unsupported Network");
+        }
 
         vm.stopBroadcast();
 
